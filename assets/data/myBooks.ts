46b0ea780a00137bd99e1ec0e561c39e4f3bfa77
heyerinnn <52750797+heyerinnn@users.noreply.github.com>
2019-07-24 10:15:56 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import { BookInfo } from "../../app/bookInfo";
+
+export  const MYBOOKS: BookInfo [] = [
+    {
+        bname: "The Subtle Art of Not Giving a F*ck",
+        bauthor: "Mark Manson",
+        bgenre: "Self-help",
+        byear: 2016,
+        bimage: "assets/images/thesubtleart.jpg"
+    },
+    {
+        bname: "Le Petit Prince",
+        bauthor: "	Antoine de Saint-Exupéry",
+        bgenre: "Classic",
+        byear: 1943,
+        bimage: "assets/images/lepetitprince.jpg"
+    },
+    {
+        bname: "The Alchemist",
+        bauthor: "Paulo Coelho",
+        bgenre: "Quest, adventure, fantasy",
+        byear: 1988,
+        bimage: "assets/images/thealchemist.jpg"
+    },
+    {
+        bname: "The Time Traveler's Wife",
+        bauthor: "Audrey Niffenegger",
+        bgenre: "Romance, science fiction, fantasy",
+        byear: 2003,
+        bimage: "assets/images/timetravellerswife.jpg"
+    }
+]
